d7308d419a2d632ed89dc3f3d4c40464964cfc2d
Kninja99 <kcdamschen@yahoo.com>
2023-09-19 12:14:21 -0700

    UI start

---
+++ scenes/levels/inside.gd
@@ -0,0 +1,10 @@
+extends LevelParent
+
+
+
+
+func _on_exit_gate_area_body_entered(_body):
+	# stoping the player
+	var tween = create_tween()
+	tween.tween_property($Player, "speed", 0,.5)
+	get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
